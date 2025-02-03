09e6bbce2ea2ab7079b0e7df25b2a079f8cbb34f
GabrielCaja <gabrielcajaegido@gmail.com>
2025-02-03 13:42:07 +0100

    Reemplazar la visualización de productos con una tabla que incluye opciones para ver, editar y eliminar

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,31 +1,65 @@
 <template>
-    <h1>{{texto}}</h1>
-    <div v-for="producto in productos">
-        <p>{{producto.id}} - {{producto.title}}</p>
-    </div>
+    <!-- Tabla de productos -->
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Ver</th>
+                <th>Editar</th>
+                <th>Eliminar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="producto in productos" :key="producto.id">
+                <td>{{ producto.id }}</td>
+                <td>{{ producto.title }}</td>
+                <td>${{ producto.price }}</td>
+                <td>
+                    <button class="ver" @click="verProducto(producto)">
+                        Ver
+                    </button>
+                </td>
+                <td>
+                    <button class="editar" @click="editarProducto(producto)">
+                        Editar
+                    </button>
+                </td>
+                <td>
+                    <button
+                        class="eliminar"
+                        @click="eliminarProducto(producto.id)"
+                    >
+                        Eliminar
+                    </button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 
-<script> 
-import axios from 'axios';
-export default{
-    data(){
-        return  {
-            texto: 'Hola Mundo',
-            productos: []
+<script>
+import axios from "axios";
+export default {
+    data() {
+        return {
+            texto: "Hola Mundo",
+            productos: [],
         };
     },
-    mounted(){
+    mounted() {
         this.obtenerProductos();
     },
     methods: {
-        async obtenerProductos(){
-            const response = await axios.get('/api/products');
+        async obtenerProductos() {
+            const response = await axios.get("/api/products");
             try {
                 this.productos = response.data;
             } catch (error) {
                 console.error(error);
             }
         },
-    }
-}
+    },
+};
 </script>
